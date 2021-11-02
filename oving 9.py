230df193c0ab94f727c36ba0481a77ef3742354d
ole <olek.valhammer@live.no>
2021-11-02 20:02:27 +0100

    j

--- oving 9.py
+++ oving 9.py
@@ -1,42 +1,102 @@
 
-myDictIsVeryAverage = {}
-c = 0
 
 
-with open("sporsmaal.txt", "r") as file:
-    row = file.readline
-    for i in row:
-        print(i)
-
-"""
 class Sporsmaal:
-    def __init__(self, spørsmål, altA, altB, altC, altD, svar):
-        self.spørsmål = spørsmål
-        self.svar = svar
-        self.altA = altA
-        self.altB = altB
-        self.altC = altC
-        self.altD = altD
-    
+    def __init__(self, quizText="", correctAnswer=None, alternatives=""):
+        self.quizText = quizText
+        self.correctAnswer = correctAnswer
+        self.alternatives = alternatives
+
     def __str__(self):
+        print("\n\n")
+        a = self.quizText+" \n"
+        for i in self.alternatives:
+            a+="\n"+i
+        return a
         
-        return ("\n",str(self.spørsmål), " \n", "1. ", str(self.altA), "2. ", str(self.altB), "3. ", str(self.altC), "4. ", str(self.altD)," \n")
-    
-    def sjekk_svar(self):
-        userInput = int(input("Ditt svar (1..4): "))
-        if userInput == self.svar:
-            print("Korrekt\n")
-        else:
-            if rundeTo:
-                if userInput == 4:
-                    print("Subjektivt heilt rett, men feil i denna quizzen.. :( ")
+
+    def korrekt_svar_tekst(self):
+        korrektSvar = int(self.correctAnswer)+1
+        return korrektSvar
+
+
+
+
+def getValues():
+    questionList = []
+    with open("sporsmaalsfil.txt", "r") as file:
+        row = file.readlines()
+        for i in row:
+            ans = i.split(":")        
+            altList = (ans[2].split(", "))
+            questionList.append(Sporsmaal(ans[0], ans[1], altList))
+    return questionList
+
+
+
+
+
+try:
+    if __name__ == "__main__":
+        player1 = 0
+        player2 = 0
+        round = getValues()
+
+        for i in round:
+            print(i)
+            p1Answer = int(input("Player 1 answer: "))
+            p2Answer = int(input("\nPlayer 2 answer: "))
+            correctAnswer = i.korrekt_svar_tekst()
+            print("\n\nDet retta svaret va ", correctAnswer)
+            if (p1Answer) == correctAnswer:
+                player1+=1
+                print("\nPlayer 1 was correct")
             else:
-                print("Feil\n")
+                print("\nPlayer 1 was incorrect")
+            if p2Answer == correctAnswer:
+                player2+=1
+                print("\nPlayer 2 was correct")
+            else:
+                print("\nPlayer 2 was incorrect")
+        if player1 > player2:
+            print("Player 1 won, player 2 git gud")
+        elif player1 < player2:
+            print("Player 2 won, player 1 git gud")
+        else:
+            print("Draw, ggwp ")
+        print("Player 1: ", player1, " pts")
+        print("Player 2: ", player2, " pts")
+            
+except:
+    print("Please enter actual numbers, you silly goose")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 """
-
-
-
-
-
-
-
+num_lines = sum(1 for line in open("sporsmaalsfil.txt"))
+rounds = []
+for i in range(num_lines):
+    round.append([])
+"""
